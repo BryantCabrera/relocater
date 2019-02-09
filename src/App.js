c2c21b6123204960c70ed720e22574c244ffc239
Kevin <k.cuison@gmail.com>
2019-02-08 16:52:04 -0800

    Adds kevins App.js title edit.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Relocater Project New</h1>
+        <h1>Relocater Project New Title</h1>
       </div>
     );
   }
