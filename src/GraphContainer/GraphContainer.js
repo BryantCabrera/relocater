cff25ce6f76d9d8be696a890a7a01464a8d29d0b
Kevin <k.cuison@gmail.com>
2019-02-08 19:01:29 -0800

    Adds kevin adds dummy login -- rendering graph container

---
+++ src/GraphContainer/GraphContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GraphContainer = () => (
+
+    <div>
+        <div className="g-container">
+
+        <p>this is the graph container</p>
+        </div>
+    </div>
+
+)
+
+export default GraphContainer
