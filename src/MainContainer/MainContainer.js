60fa20daa8cc77f861980ffc5bb08acf100a8985
Kevin <k.cuison@gmail.com>
2019-02-11 15:10:06 -0800

    Adds kevin adds proxy and maincontainer to get county

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -5,17 +5,44 @@ import TopoMap from '../TopoMap/TopoMap'
 
 
 class MainContainer extends Component {
+
     onClick = (e) => {
 
     }
 
-    render (){
-        return(
+    componentDidMount() {
+        this.getCounties()
+    }
+
+    getCounties = async () => {
+        try {
+            const response = await fetch (`/api/socrata/Alameda`)
+            console.log(response, 'hit')
+
+            if(!response.ok) {
+                throw Error(response.statusText)
+            }
+            const countiesParsed =  await response.json()
+            console.log(countiesParsed)
+
+            this.setState({
+                counties: countiesParsed.data
+            })
+
+
+        } catch (err) {
+            console.log(err)
+            return err
+        }
+    }
+
+    render() {
+        return (
             <div>
                 <div className="m-container">
 
-                <p>this is the main container</p>
-                    <GraphButton/>
+                    <p>this is the main container</p>
+                    <GraphButton />
                     <TopoMap />
                 </div>
             </div>
