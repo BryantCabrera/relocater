fa684a4f3f24899ddc249fd7710d3d698def02d1
Bryant <cabrera.bryant@gmail.com>
2019-02-11 15:28:06 -0800

    Adds Bryant's routes updates.

--- routers/counties.js
+++ routers/counties.js
@@ -4,27 +4,27 @@ const County = require('../models/county');
 
 // index route
 router.get('/', async (req, res, next) => {
-    // req.body this is from the fetch request
-    // console.log(req.body, ' this is get all')
-       try  {
-        const allCounties = await County.find();
-          res.json({
-          status: 200,
-          data: allCounties
-        });
-      } catch (err) { 
-        res.send(err)
-      }
+  // req.body this is from the fetch request
+  // console.log(req.body, ' this is get all')
+    try  {
+    const allCounties = await County.find();
+      res.json({
+      status: 200,
+      data: allCounties
+    });
+  } catch (err) { 
+    res.send(err)
+  }
 });
 
 // show route
 router.get('/:id', async (req, res, next) => {
   try  {
-      const foundCounty = await County.findById(req.params.id);
-      res.json({
-          status: 200,
-          data: foundCounty
-      });
+    const foundCounty = await County.findById(req.params.id);
+    res.json({
+        status: 200,
+        data: foundCounty
+    });
   } catch (err){
   res.send(err);
   }
