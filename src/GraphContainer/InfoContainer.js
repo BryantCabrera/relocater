f1a0eecf1440891438e25be0af70f76385d19108
Ummer Khan <ummernkhangit@gmail.com>
2019-02-13 13:05:09 -0800

    changed class to className

--- src/GraphContainer/InfoContainer.js
+++ src/GraphContainer/InfoContainer.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import LineGraph from './LineGraph'
-import GraphContainer from './GraphContainer'
 import data from '../data/db.json'
-
-
 
 class InfoContainer extends Component {
     render = () => {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].County)
+            // console.log(data[i].County)
             if (data[i].County == this.props.countyName ) {
-                console.log(data[i].County);
+                // console.log(data[i].County);
                 return (
                     <div>
-                        <h2>Population {data[i].Population}</h2>
-                        <h2>Unemployment Rate {data[i].Unemployment_Rate}%</h2>
-                        <h2>Average House Cost ${data[i].Avg_Home_Price}</h2>
-                        <h2>Median Age {data[i].Median_Age}</h2>
-                        <h2>Living Wage ${data[i].Living_Wage}</h2>
-                        <h2>Poverty Wage ${data[i].Poverty_Wage}</h2> 
+                        <br></br><br></br><br></br><br></br><br></br><br></br>
+                        <h2>Population <br></br>{data[i].Population}</h2>
+                        <h2>Unemployment Rate <br></br>{data[i].Unemployment_Rate}%</h2>
+                        <h2>Average House Cost<br></br> ${data[i].Avg_Home_Price}</h2>
+                        <h2>Median Age<br></br> {data[i].Median_Age}</h2>
+                        <h2>Living Wage<br></br> ${data[i].Living_Wage}</h2>
+                        <h2>Poverty Wage<br></br> ${data[i].Poverty_Wage}</h2> 
                     </div>
                 )
             }                      
