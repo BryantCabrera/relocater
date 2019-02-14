42fac10e79dfb0d309299bdcef8274e6e11d8004
Kevin <k.cuison@gmail.com>
2019-02-14 14:07:13 -0800

    Adds bryants css

--- src/GraphContainer/InfoContainer.js
+++ src/GraphContainer/InfoContainer.js
@@ -7,13 +7,13 @@ class InfoContainer extends Component {
         for (let i = 0; i < data.length; i++) {
             if (data[i].County == this.props.countyName ) {
                 return (
-                    <div>
-                        <h2 className="info-container-items">Population <br></br>{data[i].Population}</h2>
-                        <h2 className="info-container-items">Unemployment Rate <br></br>{data[i].Unemployment_Rate}%</h2>
-                        <h2 className="info-container-items">Average House Cost<br></br> ${data[i].Avg_Home_Price}</h2>
-                        <h2 className="info-container-items">Median Age<br></br> {data[i].Median_Age}</h2>
-                        <h2 className="info-container-items">Living Wage<br></br> ${data[i].Living_Wage}</h2>
-                        <h2 className="info-container-items">Poverty Wage<br></br> ${data[i].Poverty_Wage}</h2> 
+                    <div className='data__info--container'>
+                        <h4 className="info-container-items">Population <br></br>{data[i].Population}</h4>
+                        <h4 className="info-container-items">Unemployment Rate <br></br>{data[i].Unemployment_Rate}%</h4>
+                        <h4 className="info-container-items">Average House Cost<br></br> ${data[i].Avg_Home_Price}</h4>
+                        <h4 className="info-container-items">Median Age<br></br> {data[i].Median_Age}</h4>
+                        <h4 className="info-container-items">Living Wage<br></br> ${data[i].Living_Wage}</h4>
+                        <h4 className="info-container-items">Poverty Wage<br></br> ${data[i].Poverty_Wage}</h4> 
                     </div>
                 )
             }                      
