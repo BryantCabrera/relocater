3e3c0ef82137fddec9bea63364606b9907dfa394
Ummer Khan <ummernkhangit@gmail.com>
2019-02-13 17:59:28 -0800

    Adds ummer's email info

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -13,25 +13,25 @@ const Contact = () => {
                 <h4>BRYANT CABRERA</h4>
                 <div><a href="https://github.com/BryantCabrera"><img class="github-logo" src="../../github-logo.png"></img></a></div>
                 <div><a href="https://www.linkedin.com/in/bryantcabrera/"><img class="linkedin-logo" src="../../linkedin-logo.png"></img></a></div><br></br>
-                <div>email</div>
+                <div>cabrera.bryant@gmail.com</div>
             </div>
             <div id="kevin">
                 <h4>KEVIN CUISON</h4>
                 <div><a href="https://github.com/kv-n"><img class="github-logo" src="../../github-logo.png"></img></a></div>
                 <div><a href="https://www.linkedin.com/in/kevincuison/"><img class="linkedin-logo" src="../../linkedin-logo.png"></img></a></div><br></br>
-                <div>email</div>
+                <div>kevins email goes here</div>
             </div>
             <div id="ummer">
                 <h4>UMMER KHAN</h4>
                 <div><a href="https://github.com/khanummer"><img class="github-logo" src="../../github-logo.png"></img></a></div>
                 <div><a href="https://www.linkedin.com/in/khanummer/"><img class="linkedin-logo" src="../../linkedin-logo.png"></img></a></div><br></br>
-                <div>UMMERNKHAN@GMAIL.COM</div>
+                <div>ummernkhan@gmail.com</div>
             </div>
             <div id="peter">
                 <h4>PETER FEINMAN</h4>
                 <div><a href="https://github.com/pfeinman"><img class="github-logo" src="../../github-logo.png"></img></a></div>
                 <div><a href="https://www.linkedin.com/in/pfeinman/"><img class="linkedin-logo" src="../../linkedin-logo.png"></img></a></div><br></br>
-                <div>email</div>
+                <div>peters email goes here</div>
             </div>   
 
         </div>
