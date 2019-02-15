fe37c1b6b293e9db0d987d8c1de5d60fb31a9eb9
Bryant <cabrera.bryant@gmail.com>
2019-02-14 21:58:50 -0800

    Adds Bryant's revision/cleaning for all files in models directory.

--- models/user.js
+++ models/user.js
@@ -11,30 +11,4 @@ const UserSchema = new mongoose.Schema({
   counties: [ { type: mongoose.Schema.ObjectId, ref: "County"} ]
 });
 
-//YouTube tutorial schema
-// const UserSchema = new mongoose.Schema({
-//   method: {
-//     type: String,
-//     enum: ['local', 'google'],
-//     required: true
-//   },
-//   local: {
-//     username: String,
-//     email: String,
-//     password: String,
-//     userCounty: String,
-//     userIncome: Number,
-//     counties: [ { type: mongoose.Schema.ObjectId, ref: "County"} ]
-//   },
-//   google: {
-//     username: String,
-//     email: String,
-//     googleId: String,
-//     userCounty: String,
-//     userIncome: Number,
-//     counties: [ { type: mongoose.Schema.ObjectId, ref: "County"} ]
-//   },
-  
-// });
-
 module.exports = mongoose.model('User', UserSchema);
