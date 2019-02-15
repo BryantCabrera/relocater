ba65c434807e124469783e907e391b20af910311
Bryant <cabrera.bryant@gmail.com>
2019-02-14 22:55:58 -0800

    Adds Bryant's revision/cleaning for all files in src > MainContainer.

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import TopoMap from '../TopoMap/TopoMap'
-
-
+import TopoMap from '../TopoMap/TopoMap';
 
 class MainContainer extends Component {
-
-
     render() {
         return (
             <div>
                 <div className="m-container">
                     <TopoMap history={this.props.history}/>
-                    
                 </div>
             </div>
-
-
         )
     }
 }
 
-export default MainContainer
+export default MainContainer;
