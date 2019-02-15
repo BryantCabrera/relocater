57f498174b35a696d7bb3afb957281d7554c89ef
Bryant <cabrera.bryant@gmail.com>
2019-02-15 10:31:48 -0800

    Add mlab mongoDB variables in .env file.

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const connectionString = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://localhost/relocater', {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
